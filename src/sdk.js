cda39a73004b00d95139c8ded69a88b34a14b5cf
Matt Privman <matt@smartcar.com>
2016-01-11 08:38:26 -0800

    Refactor SDK

--- src/sdk.js
+++ src/sdk.js
@@ -1,77 +1,145 @@
-// Smartcar SDK
+// Smartcar JS SDK
 
-var SDK = (function(window, undefined) {
-  var SDK = {};
+var Smartcar = (function(window, undefined) {
+  var Smartcar = {};
 
-  var oemConfig = {
+  // OEM static configuration
+  Smartcar.oemConfig = {
     tesla: {  color: '#CC0000' },
-    ford: {   color: '#2E9BDA' },
-    bmw: {    color: '#003399' },
+    ford: {   color: '#003399' },
+    bmw: {    color: '#2E9BDA' },
     lexus: {  color: '#5B7F95' },
     volvo: {  color: '#000F60' }
   };
 
-  SDK.init = function(options) {
+  // Sets default popup window size
+  var wnd_settings = {
+    width: 430,
+    height: 500,
+  };
+
+  wnd_settings.left = window.screenX + (window.outerWidth - wnd_settings.width) / 2;
+  wnd_settings.top = window.screenY + (window.outerHeight - wnd_settings.height) / 8;
+
+  var wnd_options = 'width=' + wnd_settings.width + ',height=' + wnd_settings.height;
+  wnd_options += ',toolbar=0,scrollbars=1,status=1,resizable=1,location=1,menuBar=0';
+  wnd_options += ',left=' + wnd_settings.left + ',top=' + wnd_settings.top;
+
+  /**
+   * Initializes Smartcar Object
+   *
+   * @param  {String} options the sdk configuration object
+   * @param  {String} options.clientId app client ID
+   * @param  {String} options.redirectUri app redirect URI
+   * @param  {String} options.selector id of buttons container div
+   * @param  {String} options.scope app oauth scope
+   * @param  {String} options.grantType oauth grant type -> defaults to 'code'
+   * @param  {Boolean} options.popup shows oauth dialog popup if true
+   * @param  {Array} options.oems oems to generate buttons for
+   * @param  {Boolean} options.approvalPrompt forces permission screen if true
+   * @param  {Function} options.callback optional callback
+   */
+  Smartcar.init = function(options) {
     this.clientId = options.clientId;
     this.redirectUri = options.redirectUri;
     this.scope = options.scope;
-    this.oems = options.oems || Object.keys(oemConfig);
-    this.buttons();
+    this.selector = options.selector;
+    this.grantType = options.grantType || 'code';
+    this.popup = options.popup || true;
+    this.oems = options.oems || Object.keys(Smartcar.oemConfig);
+    this.approvalPrompt = options.forcePrompt ? 'force' : 'auto';
+
+    if(options.callback) {
+      options.callback();
+    }
   };
 
-  SDK.generateLinks = function() {
-    var self = this;
-    return this.oems.map(function(oemName) {
-      var oem = oemConfig[oemName];
-      oem.name = oemName;
-      oem.link = 'https://' + oemName
-        + '.smartcar.com/oauth/authorize?'
-        + 'response_type=token'
-        + '&client_id=' + self.clientId
-        + '&redirect_uri=' + encodeURIComponent(self.redirectUri)
-        + '&scope=' + encodeURIComponent(self.scope.join(' '))
-        + '&approval_prompt=auto';
-      return oem;
+  /**
+   * Generates an object with OAuth URL given a single OEM name
+   *
+   * @param  {String} oemName tesla|ford|bmw|lexus|volvo
+   * @return {Object} oem object with link property
+   */
+  Smartcar.generateLink = function(oemName) {
+    var oem = Smartcar.oemConfig[oemName];
+    oem.name = oemName;
+    oem.link = 'https://' + oemName
+      + '.smartcar.com/oauth/authorize?'
+      + 'response_type=' + this.grantType
+      + '&client_id=' + this.clientId
+      + '&redirect_uri=' + encodeURIComponent(this.redirectUri)
+      + '&scope=' + encodeURIComponent(this.scope.join(' '))
+      + '&approval_prompt=' + this.approvalPrompt;
+    return oem;
+  };
+
+  /**
+   * Returns mapped out oem object with links
+   *
+   * @param  {Object} oems
+   * @return {Array} array of oem objects
+   */
+  Smartcar.generateLinks = function(oems) {
+    return oems.map(function(oemName) {
+      return Smartcar.generateLink(oemName);
     });
   };
 
-  SDK.buttons = function() {
-    var self = this, html = '';
+  /**
+   * Create buttons and insert into DOM
+   *
+   * @param {Function} done optional callback
+   */
+  Smartcar.generateButtons = function(done) {
+    var html = '';
+    var append = document.getElementById(this.selector);
 
-    var append = document.getElementById('smartcar-buttons');
+    var oems = this.generateLinks(this.oems);
 
-    this.oems = this.generateLinks();
-    this.oems.forEach(function(oem) {
+    oems.forEach(function(oem) {
       html += '<a id="'
-        + oem.name + 'Button" href="'
-        + oem.link
-        + '" class="button connect-button" style="background-color:'
-        + oem.color + '">Connect with '
-        + oem.name + '</a>';
+        + oem.name + '-button" href="' + oem.link
+        + '" class="button connect-button" style="color: #FBFBFB;'
+        + 'border:0;display:block;font-size:15px;background-color:'
+        + oem.color
+        + '">Connect with ' + oem.name + '</a>';
     });
 
     append.innerHTML = html;
 
-    this.oems.forEach(function(oem) {
-      document.getElementById(oem.name + 'Button').addEventListener('click', function(event) {
-        event.preventDefault();
-        var wnd_settings = {
-          width: 430,
-          height: 500,
-        };
-        wnd_settings.left = window.screenX + (window.outerWidth - wnd_settings.width) / 2;
-        wnd_settings.top = window.screenY + (window.outerHeight - wnd_settings.height) / 8;
-        var wnd_options = 'width=' + wnd_settings.width + ',height=' + wnd_settings.height;
-        wnd_options += ',toolbar=0,scrollbars=1,status=1,resizable=1,location=1,menuBar=0';
-        wnd_options += ',left=' + wnd_settings.left + ',top=' + wnd_settings.top;
+    // Register popup events if enabled
+    if(this.popup) {
+      Smartcar.registerPopups(oems);
+    }
 
-        window.open(this.href, 'Login with ' + oem.name, wnd_options);
-      });
-    });
-
+    if(done) {
+      done();
+    }
   };
 
-  SDK.callback = function() {};
+  /**
+   * Registers popup click handlers for given OEM objects
+   *
+   * @param  {Array} oems oem array
+   */
+  Smartcar.registerPopups = function(oems) {
+    oems.forEach(function(oem) {
+      document.getElementById(oem.name + '-button').addEventListener('click', function(event) {
+        event.preventDefault();
+        Smartcar.openDialog(oem.name);
+      });
+    });
+  };
 
-  return SDK;
+  /**
+   * Opens a dialog for a specific OEM
+   *
+   * @param  {String} oemName oem name
+   */
+  Smartcar.openDialog = function(oemName) {
+    var href = this.generateLink(oemName).link;
+    window.open(href, 'Login with ' + oemName, wnd_options);
+  };
+
+  return Smartcar;
 })(window);
