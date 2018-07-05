74b06a2f916f0d01aeb0677b4d1b5328d695fec3
Nathan Shelly <nsnshelly@gmail.com>
2018-07-05 15:01:56 -0700

    feat(redirect): modify callback.js to use postMessage
    BREAKING CHANGE: callback.js now uses the `postMessage` Web API to
    interact with the parent window instead of using `window.opener`. This
    makes it much easier to use the SDK with single page applications where
    it is common for the application and the back-end to live on different
    origins. To upgrade all that needs to be done is to switch from
    callback.js to redirect.js.
    
    As a note this breaks compatibility for all browsers that do not support
    `postMessage`. See https://caniuse.com/#feat=x-doc-messaging for more
    details on supported browsers.

---
+++ src/redirect.js
@@ -0,0 +1,40 @@
+/**
+ * If you use a Smartcar-hosted redirect this code is served automatically. If
+ * you host your own redirect you can source this file (along with sourcing the
+ * Javascript SDK on your front end) to close out the redirect and trigger
+ * specified `onComplete`
+ *
+ * If the redirect URI has a valid `app_origin` param (an HTTPS URL) the
+ * extracted code will be posted to the origin specified by `app_origin`.
+ *
+ * If no `app_origin` parameter is given (only possible when self hosting,
+ * this parameter is required for using a Smartcar-hosted redirect) then
+ * the `code` (or error) is posted to the same origin the redirect is hosted
+ * at (this assumes a server side rendered architecture).
+ */
+
+(function(window) {
+  'use strict';
+  if (!window.opener) {
+    throw new Error('window.opener must be defined');
+  }
+
+  const params = new URLSearchParams(window.location.search);
+  const origin = window.location.origin;
+
+  const message = {
+    name: 'SmartcarAuthMessage',
+    isSmartcarHosted: origin === 'https://javascript-sdk.smartcar.com',
+    code: params.get('code'),
+    error: params.get('error'),
+    errorDescription: params.get('error_description'),
+    state: params.get('state'),
+  };
+
+  // if no `app_origin` given, post to same origin as redirect page (assuming
+  // server side rendered architecture)
+  const targetOrigin = params.get('app_origin') || origin;
+
+  window.opener.postMessage(message, targetOrigin);
+  window.close();
+})(window);
