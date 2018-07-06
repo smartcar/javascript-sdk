5cf1ec0dc80ebcbedf05d81671002f0cfeeb8c99
Nathan Shelly <nsnshelly@gmail.com>
2018-07-05 22:10:53 -0700

    test(sdk): update for postMessage usage

--- test/sdk.test.js
+++ test/sdk.test.js
@@ -3,6 +3,7 @@
 require('../src/sdk.js');
 
 describe('sdk', () => {
+  const CDN_ORIGIN = 'https://javascript-sdk.smartcar.com';
 
   beforeEach(() => { window.Smartcar._hasBeenInstantiated = false; });
 
@@ -30,35 +31,89 @@ describe('sdk', () => {
         );
     });
 
-    test('initializes correctly', () => {
-      const options = {
-        clientId: 'clientId',
-        redirectUri: 'https://smartcar.com',
-        scope: ['read_vehicle_info', 'read_odometer'],
-        onComplete: jest.fn(),
-      };
-
-      const smartcar = new window.Smartcar(options);
-
-      Object.entries(options).forEach(
-        ([key, option]) => expect(smartcar[key]).toEqual(option)
-      );
-
-      // this is set within the constructor
-      expect(smartcar.responseType).toEqual('code');
-      expect(smartcar.development).toEqual(false);
-
-      // make sure onComplete can be called
-      smartcar.onComplete();
-      expect(options.onComplete).toHaveBeenCalled();
-
-      expect(window.Smartcar._hasBeenInstantiated).toEqual(true);
+    test('throws error if using Smartcar hosting without onComplete', () => {
+      expect(() => new window.Smartcar({
+        redirectUri: CDN_ORIGIN,
+        clientId: 'my-id',
+      }))
+        .toThrow(
+          "When using Smartcar's CDN redirect an onComplete function with at" +
+          ' least 2 parameters (error & code) is required to handle' +
+          ' completion of authorization flow'
+        );
     });
+
+    test(
+      // eslint-disable-next-line max-len
+      'throws error if using Smartcar hosting & passing onComplete with less than 2 parameters',
+      () => {
+        expect(() => window.Smartcar({
+          redirectUri: CDN_ORIGIN,
+          clientId: 'my-id',
+          // eslint-disable-next-line no-unused-vars, no-empty-function
+          onComplete: (_) => {}, // stub function w/ < 2 params
+        }))
+          .toThrow(
+            "When using Smartcar's CDN redirect an onComplete function with at" +
+            ' least 2 parameters (error & code) is required to handle' +
+            ' completion of authorization flow'
+          );
+      }
+    );
+
+    test('initializes correctly w/ self hosted redirect',
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: 'https://selfhosted.com',
+          scope: ['read_vehicle_info', 'read_odometer'],
+          onComplete: jest.fn(),
+        };
+
+        const smartcar = new window.Smartcar(options);
+
+        Object.entries(options).forEach(
+          ([key, option]) => expect(smartcar[key]).toEqual(option)
+        );
+
+        // this is set within the constructor
+        expect(smartcar.responseType).toEqual('code');
+        expect(smartcar.development).toEqual(false);
+
+        // make sure onComplete can be called
+        smartcar.onComplete();
+        expect(options.onComplete).toBeCalled();
+      });
+
+    test('initializes correctly w/ smartcar CDN hosted redirect',
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: `${CDN_ORIGIN}/redirect?app_origin=https://app.com`,
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-unused-vars, no-empty-function
+          onComplete: jest.fn((_, __) => {}), // stub function w/ >= 2 params
+        };
+
+        const smartcar = new window.Smartcar(options);
+
+        Object.entries(options).forEach(
+          ([key, option]) => expect(smartcar[key]).toEqual(option)
+        );
+
+        // this is set within the constructor
+        expect(smartcar.responseType).toEqual('code');
+        expect(smartcar.development).toEqual(false);
+
+        // make sure onComplete can be called
+        smartcar.onComplete();
+        expect(options.onComplete).toBeCalled();
+      });
 
     test('onComplete undefined if not specified', () => {
       const options = {
         clientId: 'clientId',
-        redirectUri: 'https://smartcar.com',
+        redirectUri: 'https://selfhosted.com',
         scope: ['read_vehicle_info', 'read_odometer'],
       };
 
@@ -67,6 +122,336 @@ describe('sdk', () => {
       expect(smartcar.onComplete).toBe(undefined);
     });
 
+    test("doesn't break if onComplete is not passed", () => {
+      const options = {
+        clientId: 'clientId',
+        redirectUri: 'https://selfhosted.com',
+        scope: ['read_vehicle_info', 'read_odometer'],
+      };
+
+      const smartcar = new Smartcar(options);
+
+      const evnt = {
+        data: {
+          name: 'SmartcarAuthMessage',
+          isSmartcarHosted: false,
+          code: 'super-secret-code',
+          error: undefined,
+          state: 'some-state',
+        },
+        origin: 'https://selfhosted.com',
+      };
+
+      smartcar.messageHandler(evnt);
+    });
+
+    test("doesn't fire onComplete w/o origin", () => {
+      const options = {
+        clientId: 'clientId',
+        redirectUri: 'https://selfhosted.com',
+        scope: ['read_vehicle_info', 'read_odometer'],
+        // eslint-disable-next-line no-empty-function
+        onComplete: jest.fn(() => {}),
+      };
+
+      const smartcar = new window.Smartcar(options);
+
+      const evnt = {
+        data: {
+          name: 'SmartcarAuthMessage',
+          isSmartcarHosted: false,
+          code: 'super-secret-code',
+          error: undefined,
+          state: 'some-state',
+        },
+      };
+
+      smartcar.messageHandler(evnt);
+
+      expect(smartcar.onComplete)
+        .not
+        .toBeCalledWith(null, expect.anything(), expect.anything());
+    });
+
+    test("doesn't fire onComplete when redirectUri & origin disagree", () => {
+      const options = {
+        clientId: 'clientId',
+        redirectUri: 'https://selfhosted.com',
+        scope: ['read_vehicle_info', 'read_odometer'],
+        // eslint-disable-next-line no-empty-function
+        onComplete: jest.fn(() => {}),
+      };
+
+      const smartcar = new window.Smartcar(options);
+
+      const evnt = {
+        data: {
+          name: 'SmartcarAuthMessage',
+          isSmartcarHosted: false,
+          code: 'super-secret-code',
+          error: undefined,
+          state: 'some-state',
+        },
+        origin: 'https://some-other-url.com',
+      };
+
+      smartcar.messageHandler(evnt);
+
+      expect(smartcar.onComplete)
+        .not
+        .toBeCalledWith(null, expect.anything(), expect.anything());
+    });
+
+    test(
+      "doesn't fire onComplete or error when event.data is undefined",
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: 'https://selfhosted.com',
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-empty-function
+          onComplete: jest.fn(() => {}),
+        };
+
+        const smartcar = new window.Smartcar(options);
+
+        const evnt = {
+          origin: 'https://selfhosted.com',
+        };
+
+        smartcar.messageHandler(evnt);
+
+        expect(smartcar.onComplete)
+          .not
+          .toBeCalledWith(null, expect.anything(), expect.anything());
+      }
+    );
+
+    test(
+      "doesn't fire onComplete when message has no name field",
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: 'https://selfhosted.com',
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-empty-function
+          onComplete: jest.fn(() => {}),
+        };
+
+        const smartcar = new window.Smartcar(options);
+
+        const evnt = {
+          data: {
+            isSmartcarHosted: false,
+            code: 'super-secret-code',
+            error: undefined,
+            state: 'some-state',
+          },
+          origin: 'https://selfhosted.com',
+        };
+
+        smartcar.messageHandler(evnt);
+
+        expect(smartcar.onComplete)
+          .not
+          .toBeCalledWith(null, expect.anything(), expect.anything());
+      }
+    );
+
+    test(
+      "doesn't fire onComplete when message.name is not 'SmartcarAuthMessage'",
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: 'https://selfhosted.com',
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-empty-function
+          onComplete: jest.fn(() => {}),
+        };
+
+        const smartcar = new window.Smartcar(options);
+
+        const evnt = {
+          data: {
+            name: 'definitely not SmartcarAuthMessage',
+            isSmartcarHosted: false,
+            code: 'super-secret-code',
+            error: undefined,
+            state: 'some-state',
+          },
+          origin: 'https://selfhosted.com',
+        };
+
+        smartcar.messageHandler(evnt);
+
+        expect(smartcar.onComplete)
+          .not
+          .toBeCalledWith(null, expect.anything(), expect.anything());
+      }
+    );
+
+    test(
+      // eslint-disable-next-line max-len
+      'fires onComplete when redirectUri & origin agree, & message.name is SmartcarAuthMessage',
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: `${CDN_ORIGIN}?app_origin=https://app.com`,
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-unused-vars, no-empty-function
+          onComplete: jest.fn((__, _) => {}),
+        };
+
+        const smartcar = new window.Smartcar(options);
+
+        const evnt = {
+          data: {
+            name: 'SmartcarAuthMessage',
+            isSmartcarHosted: true,
+            code: 'super-secret-code',
+            error: null,
+            errorDescription: null,
+            state: 'some-state',
+          },
+          origin: CDN_ORIGIN,
+        };
+
+        smartcar.messageHandler(evnt);
+
+        expect(smartcar.onComplete)
+          .toBeCalledWith(null, expect.anything(), expect.anything());
+      }
+    );
+
+    test('fires onComplete w/o error when error: null in postMessage', () => {
+      const options = {
+        clientId: 'clientId',
+        redirectUri: `${CDN_ORIGIN}?app_origin=https://app.com`,
+        scope: ['read_vehicle_info', 'read_odometer'],
+        // eslint-disable-next-line no-unused-vars, no-empty-function
+        onComplete: jest.fn((__, _) => {}),
+      };
+
+      const smartcar = new window.Smartcar(options);
+
+      const evnt = {
+        data: {
+          name: 'SmartcarAuthMessage',
+          isSmartcarHosted: true,
+          code: 'super-secret-code',
+          error: null,
+          state: 'some-state',
+        },
+        origin: CDN_ORIGIN,
+      };
+
+      smartcar.messageHandler(evnt);
+
+      expect(smartcar.onComplete)
+        .toBeCalledWith(null, 'super-secret-code', 'some-state');
+    });
+
+    test('fires onComplete w/o error when error key not in postMessage', () => {
+      const options = {
+        clientId: 'clientId',
+        redirectUri: `${CDN_ORIGIN}?app_origin=https://app.com`,
+        scope: ['read_vehicle_info', 'read_odometer'],
+        // eslint-disable-next-line no-unused-vars, no-empty-function
+        onComplete: jest.fn((__, _) => {}),
+      };
+
+      const smartcar = new window.Smartcar(options);
+
+      const evnt = {
+        data: {
+          name: 'SmartcarAuthMessage',
+          isSmartcarHosted: true,
+          code: 'super-secret-code',
+          errorDescription: 'this doesnt matter',
+          state: 'some-state',
+        },
+        origin: CDN_ORIGIN,
+      };
+
+      smartcar.messageHandler(evnt);
+
+      expect(smartcar.onComplete)
+        .toBeCalledWith(null, 'super-secret-code', 'some-state');
+    });
+
+    test(
+      // eslint-disable-next-line max-len
+      'fires onComplete w/ AccessDenied error when `error: access_denied` in postMessage',
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: `${CDN_ORIGIN}?app_origin=https://app.com`,
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-unused-vars, no-empty-function
+          onComplete: jest.fn((__, _) => {}),
+        };
+
+        const smartcar = new window.Smartcar(options);
+        const errorDescription = 'describes the error';
+
+        const evnt = {
+          data: {
+            name: 'SmartcarAuthMessage',
+            isSmartcarHosted: true,
+            code: 'super-secret-code',
+            error: 'access_denied',
+            errorDescription,
+            state: 'some-state',
+          },
+          origin: CDN_ORIGIN,
+        };
+
+        smartcar.messageHandler(evnt);
+
+        expect(smartcar.onComplete)
+          .toBeCalledWith(new window.Smartcar.AccessDenied(errorDescription),
+            'super-secret-code', 'some-state');
+      }
+    );
+
+    test(
+      // eslint-disable-next-line max-len
+      'fires onComplete w/ "Unexpected error" error when `error` key has value other than `access_denied`',
+      () => {
+        const options = {
+          clientId: 'clientId',
+          redirectUri: `${CDN_ORIGIN}?app_origin=https://app.com`,
+          scope: ['read_vehicle_info', 'read_odometer'],
+          // eslint-disable-next-line no-unused-vars, no-empty-function
+          onComplete: jest.fn((__, _) => {}),
+        };
+
+        const smartcar = new window.Smartcar(options);
+        const error = 'not_access_denied';
+        const errorDescription = 'describes the error';
+
+        const evnt = {
+          data: {
+            name: 'SmartcarAuthMessage',
+            isSmartcarHosted: true,
+            code: 'super-secret-code',
+            error,
+            errorDescription,
+            state: 'some-state',
+          },
+          origin: CDN_ORIGIN,
+        };
+
+        smartcar.messageHandler(evnt);
+
+        expect(smartcar.onComplete)
+          .toBeCalledWith(
+            Error(`Unexpected error: ${error} - ${errorDescription}`),
+            'super-secret-code',
+            'some-state'
+          );
+      }
+    );
   });
 
   describe('getAuthUrl', () => {
